7cbb836818a7c866c2fcfa0d4b9f9947ccfb18ee
Brian Leist <bleist88@gmail.com>
2018-07-04 15:29:31 -0400

    updating

--- astrolib/mcc/__init__.py
+++ astrolib/mcc/__init__.py
@@ -5,4 +5,4 @@ A Master Catalog Correlation Package.
 from    .master         import master
 from    .correlation    import correlate, clean_duplicates, combine
 from    .create         import create
-from    .stamp          import Stamp
+from    .stamp          import stamp
