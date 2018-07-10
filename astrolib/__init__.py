f3e0aca56d2f295d89a50688d569f7d2445c5a00
Brian Leist <bleist88@gmail.com>
2018-07-10 17:08:06 -0400

    updating

--- astrolib/__init__.py
+++ astrolib/__init__.py
@@ -3,9 +3,10 @@ A library of astronomy related packages.
 """
 
 __all__     = [
-    "io", "mcc", "photo", "imports"
+    "io", "mcc", "photo"
 ]
 
 from . import io
 from . import mcc
 from . import photo
+from . import imports
