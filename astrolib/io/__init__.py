492536997c68762f24d01d52a8a5215c52294f37
Brian Leist <bleist88@gmail.com>
2019-01-28 18:34:02 -0500

    updating

--- astrolib/io/__init__.py
+++ astrolib/io/__init__.py
@@ -4,7 +4,7 @@ from .file_parsing  import parse_file, get_body, get_comments, get_dtype, \
 from .cl_parsing    import cl_parser
 from .formatting    import get_dstring, tobool
 from .data_io       import read, write, start_file, write_to, write_configs, \
-                            add_columns
+                            add_column
 from .class_io      import save_obj, open_obj
 from .figure        import smart_figure
 from .display       import progress, timer
