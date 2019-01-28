d2560c4f53dfcd50c1b0405f006f09a23418b176
Brian Leist <bleist88@gmail.com>
2019-01-28 17:50:06 -0500

    updating

--- astrolib/io/__init__.py
+++ astrolib/io/__init__.py
@@ -1,9 +1,9 @@
 
-from .file_parsing  import parse_file, get_body, get_comments, get_dtype,
+from .file_parsing  import parse_file, get_body, get_comments, get_dtype, \
                             get_configs, parse_path
 from .cl_parsing    import cl_parser
 from .formatting    import get_dstring, tobool
-from .data_io       import read, write, start_file, write_to, write_configs,
+from .data_io       import read, write, start_file, write_to, write_configs, \
                             add_columns
 from .class_io      import save_obj, open_obj
 from .figure        import smart_figure
